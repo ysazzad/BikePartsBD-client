c7e8dbcc17fb24532fd6abd9c6b5c876eda9bfdf
sazzad <hossensazzad1201@gmail.com>
2022-05-22 16:33:04 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
+      <Footer></Footer>
 
 
     </div>
