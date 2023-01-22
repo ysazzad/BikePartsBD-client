d3f3e97084825907bb9e685310d49a2baf5bbe4c
sazzad <hossensazzad1201@gmail.com>
2023-01-22 17:00:02 +0600

    change

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -4,7 +4,7 @@ import Part from './Part';
 const Parts = () => {
     const [parts, setParts] = useState([])
     useEffect(() => {
-        fetch(` https://motorcycle-parts-bd.onrender.com/part `)
+        fetch(` https://motorcycle-parts-bd.onrender.com/part`)
             .then(res => res.json())
             .then(data => setParts(data))
     }, [])
